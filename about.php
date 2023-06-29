14e044f99312b32a0cd278df56787d95fa2be440
Sonu Yadav <105367984+Gyanpie@users.noreply.github.com>
2023-06-29 19:56:33 +0530

    Add files via upload
    first commit

---
+++ about.php
@@ -0,0 +1,276 @@
+<?php include 'header.php'
+?>
+  <div class="about-hero my-10">
+    <div class="container">
+      <h2 class="title text-center">We are <span>Formula</span></h2>
+      <div class="about-banner">
+        <img src="img/about-banner.png" alt="" width="100%" class="dark-img">
+        <img src="img/about-banner1.png" alt="" width="100%" class="light-img">
+      </div>
+    </div>
+  </div>
+
+  <!-- Strat about -->
+  <div class="services-collection">
+    <div class="container">
+      <div class="row household">
+        <div class="col-md-4 household-sidebar">
+          <ul>
+            <li>
+              <a href="#Story">Story</a>
+            </li>
+            <li>
+              <a href="#Whyformula?">Why formula?</a>
+            </li>
+            <li>
+              <a href="#Formulateam">Formula team</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-8">
+          <div id="Story">
+            <h3 class="second-title">Story</h3>
+            <p class="mt-5">Formula Group is a provider of integrated corporate mobility services in India,
+              providing <span style="color: red;">mobility services to over 20 Fortune 100 clients</span> for both
+              inbound as well as outbound mobility needs.</p>
+
+
+            <div class="about-story-section">
+              <div class="row">
+                <div class="col1">
+                  <div class="storyf">
+                    <span>2004</span>
+                    <p>YEAR FOUNDED</p>
+                  </div>
+                </div>
+                <div class="col1">
+                  <img src="img/about-img.jpg" alt="" width="100%">
+                  <p>OUR JOURNEY</p>
+                </div>
+              </div>
+            </div>
+
+            <style>
+              .about-story-section .row .col1:nth-child(1) {
+                width: 339px;
+height: 178px;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    border-radius: 10px;
+    background: #F2EDE7;
+    font-size: 50px;
+    margin-right:20px
+}
+.about-story-section .row .col1:nth-child(2) {
+  width: 477px;
+height: 178px;
+    text-align: center;
+    border-radius: 10px;
+    background: #F2EDE7;
+    font-size: 50px;
+}
+.about-story-section p{
+  color:#000;
+}
+            </style>
+
+
+
+
+            <p>We truly understand how unnerving it can be to move to a new city, adjust to a new
+              culture, get children admitted to schools or for spouses to make a new daily routine.
+              Millennial employees can also find it daunting to adjust to a new culture, find safe
+              accommodation etc. However, moving to a new city is also filled with opportunities
+              and our dedicated, trained consultants help you overcome these obstacles and ensure that all you need to
+              focus on is opportunities.</p>
+
+            <p> <span style="color: red;">We organize each step of the journey to be tailored to the needs of relocating
+                employees and their families</span> and are there for them till they are able to call their new house a
+              home.</p>
+
+          </div>
+
+          <div class="formula-section my-10" id="Whyformula?">
+             <div class="row">
+                <div class="col-md-12 container col-lg-6 formula-content">
+                    <div class="">
+                        <h2 class="title">Why <span>Formula?</span></h2>
+                        <p class="formula-ex">19+ Years Experience</p>
+                        <p>Incorporated in January 2004</p>
+                        <p class="mt-4">As a prominent provider of corporate service solutions and relocation services, our company
+                            offers comprehensive "Single Window Services" for clients' relocation and fleet management
+                            requirements.</p>
+                        <p class="mt-4">Since 1989, the parent group has delivered top-notch corporate travel services to
+                            multinational companies.</p>
+                        <a href="#" class="btn2 mt-4">Learn more</a>
+                    </div>
+                </div>
+                <div class="col-md-12 col-lg-6 container formula-card-area">
+                    <div class="row">
+                        <div class="col-6 col-sm-6 mt-5">
+                            <div class="box-border2">
+                            <div class="card formula-card">
+                                <div class="formula-logo"></div>
+                                <div class="px-5">
+                                    <span>19+ years experience</span>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-6 col-sm-6">
+                            <div class="box-border2">
+                            <div class="card formula-card py-4">
+                                <div class="formula-logo2"></div>
+                                <div class="px-5">
+                                    <span>One-stop solution</span>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-sm-6 mt-5">
+                            <div class="box-border2">
+                            <div class="card formula-card py-4">
+                                <div class="formula-logo3"></div>
+                                <div class="px-5">
+                                    <span>100% safe & secure</span>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-6 col-sm-6 mt-4">
+                            <div class="box-border2">
+                            <div class="card formula-card py-4">
+                                <div class="formula-logo4"></div>
+                                <div class="px-5">
+                                    <span>On-time delivery</span>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             </div>
+          </div>
+
+
+          <div id="Formulateam">
+            <h3 class="second-title">Formula Team</h3>
+            <div class="photo-gallery-wrapper mt-5">
+              <ul class="photo-gallery-wrapper__column">
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/t1.png" alt="" />
+                </</div>div>
+                </li>
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/t4.png" alt="" />
+                  </div></div>
+                </li>
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/t7.png" alt="" />
+                  </div></div>
+                </li>
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/t2.png" alt="" />
+                  </div></div>
+                </li>
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/t5.png" alt="" />
+                  </div></div>
+                </li>
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/t3.png" alt="" />
+                  </div></div>
+                </li>
+
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/t6.png" alt="" />
+                  </div></div>
+                </li>
+
+                <li>
+                  <div class="box-border3">
+                  <div class="card">
+                  <img src="img/team/www.jpeg" alt="" style="height: 273px !important;" />
+                  </div></div>
+                </li>
+              </ul>
+            </div>
+
+            <style>
+              .photo-gallery-wrapper {
+                max-width: 1200px;
+                margin: 0px auto;
+              }
+
+              .photo-gallery-wrapper__column {
+                column-count: 3;
+                column-width: 200px;
+                list-style: none;
+                font-size: 0;
+                margin: 0;
+                padding: 0;
+                column-gap: 18px;
+                transition: ease-in 0.3s;
+              }
+
+              .photo-gallery-wrapper__column li{
+                margin-bottom: 18px;
+                border-radius: 3px;
+                overflow: hidden;
+              }
+              
+              .photo-gallery-wrapper__column li .box-border3{
+                border: 2px solid #E6E6E6;
+                padding: 20px;
+                border-radius: 0.5rem;
+                margin: 30px;
+              }
+              .photo-gallery-wrapper__column li .box-border3:hover{
+                transform: rotate(-21.683deg);
+                border: 4px solid #DF1E26;
+                z-index: 11;
+              }
+              .photo-gallery-wrapper__column li .box-border3:hover .card{
+                transform: rotate(21.683deg);
+              }
+              .photo-gallery-wrapper__column li .card img {
+                width: 100%;
+                height: 100%;
+                border-radius: 3px;
+                transition: ease-in 0.1s;
+              }
+            </style>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End about -->
+
+
+  <div class="bottom text-center my-10">
+    <p>Seeking Expert, Stress-Free Moving Services?</p>
+    <a href="#" class="btn">Contact Us</a>
+  </div>
+
+
+
+
+ <?php include 'footer.php' ?>
